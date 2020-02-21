83503d001ea8c31d84dd2e4638c404aa9b61c6a8
Cooper Standard <cooper@cooperstandad.org>
2020-02-20 18:45:33 -0700

    prompt and input work

--- shell.c
+++ shell.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "shell.h"
 #include <unistd.h>
+#include "shell.h"
+
 
 int main(){
+    int done = 0;
+    while (!done) { //program run loop, need to set up exit commands until then use keyboard interrupt to exit
 
-int count = 2;
-char* cArray = (char*) malloc(count *sizeof(char));
+        int count = 1;
+        char* cwd = (char*) malloc(count * sizeof(char));
+        while(getcwd(cwd, count) == NULL){
+            count++;
+            free(cwd);
+            cwd = (char*) malloc(count * sizeof(char));
+        }
 
- while(cArray == NULL){
-     cArray = getcwd(cArray, sizeof(cArray));
-     count++;
-     free(cArray);
-     cArray = (char*) malloc(count *sizeof(char));
- }
+        printf("%s> ", cwd);
+        int length = (count * 2) + 32; //this is arbitratry but I dont think it needs to be any bigger. If steve says otherwise can change
+        char* input = (char*) malloc(length * sizeof(char));
+        fgets(input, length + 1, stdin); //array is now formatted like "what was typed but always terminated with \n" followed by \0 until the array is full
+        printf("%s", input); // this just prints out what ^ read. once commands are working only print this if the input doesnt match a command
 
-printf("%s> \n", cArray);
-
+    }
+    
 }
 
 
